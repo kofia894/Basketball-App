1dc62c44b9eb7cf844c31cc3649d12485add9c8b
Corrado-K <kebemartey@gmail.com>
2022-09-23 11:53:33 +0000

    Updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,36 @@ import Navbar from './components/navbar'
 import Maincards from './components/maincards';
 import Higlights from './components/highlights';
 
+// Routing
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+/* Pages */
+import Home from './pages/Home';
+
+/* End of pages */
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="container md:flex">
+
+      <Router>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path={'/teams'} element={<Home />} />
+        </Routes>
+
+      </Router>
+
       <Navbar />
       
-      <div className=" flex  ">
+      {/* <div className=" flex  ">
         <Sidebar />
 
         <Maincards/>
       </div>
 
-      <Higlights />
+      <Higlights /> */}
       
       
      
